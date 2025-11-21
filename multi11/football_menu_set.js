6ec5e6f0a5ad2300be83e5cc49bdff406bf9c4e5
Anne201 <118590269+Anne201@users.noreply.github.com>
2025-11-21 11:06:29 +0800

    V1

---
+++ multi11/football_menu_set.js
@@ -0,0 +1,10 @@
+menu_title = "10以內的減法";
+menu_items = new Array(
+
+  "10- 心算,data/sub-1.js"
+, "9- 心算,data/sub-2.js"
+, "8-/7- 心算,data/sub-3.js"
+, "6-/5- 心算,data/sub-4.js"
+, "4-/3-/2-/1- 心算,data/sub-5.js"
+
+);
